d821a0e6a42374688140b42b11886c4a6bec2441
42akira <anina4242@gmail.com>
2026-07-20 13:34:08 +0200

    added two sound and img

--- soundscript.js
+++ soundscript.js
@@ -2,8 +2,11 @@
 
 
 
-const image = document.getElementById("shrek-image");
-const sound = new Audio("./material/shrek_sound.mp3");
+const image = document.getElementById("shrek");
+const sound = new Audio("audio/sound.mp3");
+
+const image1 = document.getElementById("batman");
+const sound1 = new Audio("audio/btaman_transition.wav");
 
 image.addEventListener("click", () => {
     sound.currentTime = 0;
@@ -11,4 +14,12 @@ image.addEventListener("click", () => {
         console.error("The sound could not be played:", error);
     });
 });
+
+image1.addEventListener("click", () => {
+    sound1.currentTime = 0;
+    sound1.play().catch(error => {
+        console.error("The sound could not be played:", error);
+    });
+});
+
 
